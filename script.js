85bfd2acd130a105ac26f6eca1048ddeeb3c0c1d
Sateesh <kumarsateesh838@gmail.com>
2023-09-22 23:05:52 +0530

    mkl

--- script.js
+++ script.js
@@ -35,6 +35,22 @@ no.addEventListener("mouseenter", function() {
     }
     currentAction = (currentAction + 1) % 4; 
 });
-
+no.addEventListener("click", function() {
+    switch (currentAction) {
+        case 0:
+            no.style.left = "100px";
+            break;
+        case 1:
+            no.style.top = "100px";
+            break;
+        case 2:
+            no.style.left = "0";
+            break;
+        case 3:
+            no.style.top = "0";
+            break;
+    }
+    currentAction = (currentAction + 1) % 4; 
+});
 
 
