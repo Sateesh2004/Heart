41fc2996c6457c0f3df6971ca7290cbe453392d3
Sateesh <kumarsateesh838@gmail.com>
2023-09-22 22:56:45 +0530

    again change

--- script.js
+++ script.js
@@ -38,10 +38,10 @@ no.addEventListener("mouseenter", function() {
 no.addEventListener("click", function() {
     switch (currentAction) {
         case 0:
-            no.style.left = "200px";
+            no.style.left = "100px";
             break;
         case 1:
-            no.style.top = "200px";
+            no.style.top = "100px";
             break;
         case 2:
             no.style.left = "0";
